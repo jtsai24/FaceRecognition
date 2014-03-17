8524f18472f0abe0481c82c86fc9e982ad457bca
xinquan <xinquanqu@gmail.com>
2014-03-17 12:33:17 -0400

    more description

--- cppcode/faceDetect.h
+++ cppcode/faceDetect.h
@@ -3,6 +3,18 @@
 
 #include <string>
 
+/*
+ *  Using openCV to process image, 
+ *  create a new image and save it locally
+ *. Send the path of the new image.
+ * 
+ *  Argument: 
+ *  orgImFilePath: image file stored locally
+ *  
+ *  Output:
+ *  file path of the image created. 
+**/
+
 string faceDetect (string orgImgFilePath);
 
 #endif
